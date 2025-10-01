34135e6b8f884fbaea9584e877ee848bbfe9d4a9
Shubham Hire <shubham.hire2006@gmail.com>
2025-10-01 20:35:01 +0530

    other things

--- lec6/six.js
+++ lec6/six.js
@@ -1,1 +1,15 @@
 console.log("Document Object Model");
+console.log("hello");
+
+let h2 = document.getElementById("head");
+console.dir(h2.innerText);
+ 
+h2.innerText = h2.innerText  + " from apna college";
+
+let divs = document.querySelectorAll(".box");
+
+let idx = 0;
+for (div of divs) {
+    div.innerText = `new unique value is ${idx}`;
+    idx++;
+}
