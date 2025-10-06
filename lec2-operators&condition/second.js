ccef57fa289ed5c0d468beb0ec43ef3f42a1d989
Shubham Hire <shubham.hire2006@gmail.com>
2025-10-06 19:43:26 +0530

    updates

--- lec2-operators&condition/second.js
+++ lec2-operators&condition/second.js
@@ -12,12 +12,12 @@ let a = 5;
 let b = 2 ;
 
 console.log("a = ", a , "b = ", b);
-// console.log("a + b =", a + b);
-// console.log("a - b =", a - b);
-// console.log("a * b =", a * b);
-// console.log("a / b =", a / b);
-// console.log("a % b = ", a % b);
-// console.log("a^b = ", a ** b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b = ", a % b);
+console.log("a^b = ", a ** b);
 
 // Unary operator
 
