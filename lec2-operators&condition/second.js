c9e53540cf1b95faaffe6a868b53ce514bdc95ea
Shubham Hire <shubham.hire2006@gmail.com>
2025-10-06 19:42:51 +0530

    updates

--- lec2-operators&condition/second.js
+++ lec2-operators&condition/second.js
@@ -101,21 +101,21 @@ console.log("a = ", a , "b = ", b);
 
 // Question : write a code which can give gradesbto students according to their scores
 
-let score = 40;
-let grade;
-if (score >= 90 && score <= 100)  {
-    grade = "A";
-} else if (score >= 70 && score <= 89) {
-grade = "B";
-} else if (score >= 60 && score <= 69) {
-    grade = "C";
-} else if (score >=50 && score <= 59) {
-    grade = "D";
-} else {
-    grade = "F"
-}
+// let score = 40;
+// let grade;
+// if (score >= 90 && score <= 100)  {
+//     grade = "A";
+// } else if (score >= 70 && score <= 89) {
+// grade = "B";
+// } else if (score >= 60 && score <= 69) {
+//     grade = "C";
+// } else if (score >=50 && score <= 59) {
+//     grade = "D";
+// } else {
+//     grade = "F"
+// }
 
-console.log(grade);
+// console.log(grade);
 
 
 
